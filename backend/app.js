d998b1f44e9c0401dd8b935ee01cd8191a98d111
sanguchito <2017razor2017@gmail.com>
2026-03-17 15:36:15 -0600

    Tercer COMMIT, completado casi

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 import express from "express";
+//Importamos TODOS los ENDPOINTS para ser utilizados en el endpoint principal
+import productRoutes from "./src/routes/products.js";
 
 //Crear una constante que guarda la instancia EXPRESS
 const app = express();
 
 /* ACA VAN TODOS LOS ENDPOINTS!!! */
-app.use("/api/products");
+app.use("/api/products", productRoutes);
 
 //Exportamos la función EXPRESS
 export default app;
--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -1,15 +1,18 @@
 import express from "express";
+//Importamos el controlador PARA REFERENCIAR CADA ENDPOINT
+import productsController from "../controllers/products";
 
 //Acá nos ayudará Router() que contendrá todos los endpoints
 const router = express.Router();
 
 //Router tiene todos los MÉTODOS HTTP
 router.route("/")
-.get()
-.post();
+.get(productsController.getProducts)
+.post(productsController.insertProducts);
 
+//Por ID
 router.route("/:id")
-.put()
-.delete();
+.put(productsController.updateProducts)
+.delete(productsController.deleteProducts);
 
 export default router;
