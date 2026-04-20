11e57e9fc9708097cee4748c9a1e54d2381de624
sanguchito <2017razor2017@gmail.com>
2026-04-20 08:22:35 -0600

    Controlador Fix

--- backend/src/routes/customerLogin.js
+++ backend/src/routes/customerLogin.js
@@ -1,11 +1,11 @@
 import express from "express";
 //Importamos el controlador PARA REFERENCIAR CADA ENDPOINT
-import customerLoginController from "../controllers/customerLogin.js";
+import logoutController from "../controllers/controllerLogout.js";
 
 //Acá nos ayudará Router() que contendrá todos los endpoints
 const router = express.Router();
 
 //Router tiene todos los MÉTODOS HTTP
-router.route("/").post(customerLoginController.login);
+router.route("/").post(logoutController.logout);
 
 export default router;
